e1382a45c80b98ac0079ad8ea8b8df8696b2dc7a
Coen Büller <c.m.buller@student.tudelft.nl>
2025-11-08 14:39:31 +0100

    implemented the basic GA. Now we only have to find the best parameters for n_crossover, mutation_rate, n_offspring, population size and if we want (mu+lambda) or (mu,lambda). This can be doen through calculating the maximum likelihood of improvement

--- GA.py
+++ GA.py
@@ -39,36 +39,70 @@ def crossover(pop, pop_f, n_offspring, n_crossovers):
     exp_p = np.exp(pop_f)
     weights = exp_p/(np.sum(exp_p)) # Higher probability to mate when parent has high score
 
-    mating = rs.choice(pop, size=2*n_offspring, p=weights, replace=True)
-    offspring = []
+    mating_idx = rs.choice(pop.shape[0], size=2*n_offspring, p=weights, replace=True)
+    mating = pop[mating_idx]
+
+    offspring = np.zeros(shape=(n_offspring, mating.shape[1]))
     for i in range(1, len(mating), 2):
-        offspring[i] = cross(pop[i-1], pop[i], n_cross=n_crossovers)
+        offspring[int((i-1)/2)] = cross(mating[i-1], mating[i], n_cross=n_crossovers)
     
     return offspring
 
 
-def studentnumber1_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
+def mutation(offspring, p_mutate):
+    """Helper function to apply the mutation"""
+
+    p_not_mutate = 1 - p_mutate # Probability the bit wont mutate
+    mutation_array = rs.choice(2, offspring.shape, replace=True, p=[p_not_mutate, p_mutate]) # Mutate according to np.abs(bit_val - 1) if mutate is true
+    offspring = np.abs(offspring - mutation_array).astype(int)
+    return offspring
+
+
+def selection(parents, parents_f, offspring, offspring_f, mu_plus_lambda, top_k):
+    if mu_plus_lambda:
+        population, population_f = np.vstack([parents, offspring]), np.hstack([parents_f, offspring_f])
+    else: 
+        population, population_f = offspring, offspring_f
+
+    best_idx = np.argsort(population_f)[:top_k]
+    return population[best_idx], population_f[best_idx]
+
+
+
+def s2631415_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
     # initial_pop = ... make sure you randomly create the first population
 
+    MU_PLUS_LAMBDA = True # Do we select the best of offspring and parents or only the ofspring, 'True' for both parents and offspring, and 'False' for only offspring
     MU = 10 # Population size
     LAMBDA = 20 # Offspring size
 
-    pop = rs.choice(2, (MU, problem.bounds.lb.shape[0]), replace=True)
-    pop_f = problem(pop)
+    """ These two parameters can be statistically adjusted through the process. Need a statistical derivation of what value achieves the maximum likelihood of improving the score"""
+    N_CROSSOVERS = 5 # How many places we want to crossover 
+    MUTATION_RATE = 0.02 # Chance to flip bit
+
+
+    parents = rs.choice(2, (MU, problem.bounds.lb.shape[0]), replace=True)
+    parents_f = problem(parents) # type: ignore
 
     # `problem.state.evaluations` counts the number of function evaluation automatically,
     # which is incremented by 1 whenever you call `problem(x)`.
     # You could also maintain a counter of function evaluations if you prefer.
     while problem.state.evaluations < budget:
 
+        offspring = crossover(parents, parents_f, LAMBDA, N_CROSSOVERS)
 
-        # please implement the mutation, crossover, selection here
-        # .....
+        offspring = mutation(offspring, MUTATION_RATE)
+        offspring_f = np.zeros(shape=offspring.shape[0])
+        for i in range(len(offspring)):
+            offspring_f[i] = problem(list(offspring[i]))
+        
+        parents, parents_f = selection(parents, parents_f, offspring, offspring_f, MU_PLUS_LAMBDA, MU)
+        if budget % 1000 == 0:
+            print(problem.state)
+        
         # this is how you evaluate one solution `x`
         # f = problem(x)
     # no return value needed
-        pass
-
 
 def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logger.Analyzer]:
     # Declaration of problems to be tested.
@@ -93,13 +127,13 @@ if __name__ == "__main__":
     # create the LABS problem and the data logger
     F18, _logger = create_problem(dimension=50, fid=18)
     for run in range(20): 
-        studentnumber1_studentnumber2_GA(F18)
+        s2631415_studentnumber2_GA(F18)
         F18.reset() # it is necessary to reset the problem after each independent run
     _logger.close() # after all runs, it is necessary to close the logger to make sure all data are written to the folder
 
     # create the N-Queens problem and the data logger
     F23, _logger = create_problem(dimension=49, fid=23)
     for run in range(20): 
-        studentnumber1_studentnumber2_GA(F23)
+        s2631415_studentnumber2_GA(F23)
         F23.reset()
     _logger.close()
